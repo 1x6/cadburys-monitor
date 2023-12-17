ab9cc95bb6f745af1c354a0c3c143c4d2b8a7a31
a <44981148+1x6@users.noreply.github.com>
2023-12-17 00:38:46 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+import os
+import requests
+import time
+
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/..."
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "cadburys secret santa tracker by xeny yours truly 💯💯💯"
+
+def send(message):
+    response = requests.post(DISCORD_WEBHOOK_URL, json={'content': message})
+    print("Message sent successfully." if response.status_code == 204 else f"Failed to send message. Status code: {response.status_code}")
+
+def check_all():
+    with open("links.txt", "r") as file:
+        links = file.read().splitlines()
+
+    for link in links:
+        print("trying link", link)
+        r = requests.get(link)
+        if "missed-out" not in r.url:
+            if link == "https://bit.ly/StarFreebies-Cadbury-32-ireland":
+                pass
+            else:
+                print(r.url)
+                send(f"@everyone\n\n<{link}>\n\n.")#{r.url}
+
+while True:
+    check_all()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
