dd2da0d880790ab6c8c9c5b6777a2307f25e9ecf
x <44981148+1x6@users.noreply.github.com>
2024-11-07 17:49:18 +0000

    faster

--- app.py
+++ app.py
@@ -14,19 +14,22 @@ def send(message):
     response = requests.post(DISCORD_WEBHOOK_URL, json={'content': message})
     print("Message sent successfully." if response.status_code == 204 else f"Failed to send message. Status code: {response.status_code}")
 
+def check_link(link):
+    print("trying link", link)
+    r = requests.get(link)
+    if "missed-out" not in r.url:
+        if link == "https://bit.ly/StarFreebies-Cadbury-32-ireland":
+            pass
+        else:
+            print(r.url)
+            send(f"@everyone\n\n<{link}>\n\n.")#{r.url}
+
 def check_all():
     with open("links.txt", "r") as file:
         links = file.read().splitlines()
 
-    for link in links:
-        print("trying link", link)
-        r = requests.get(link)
-        if "missed-out" not in r.url:
-            if link == "https://bit.ly/StarFreebies-Cadbury-32-ireland":
-                pass
-            else:
-                print(r.url)
-                send(f"@everyone\n\n<{link}>\n\n.")#{r.url}
+    with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        executor.map(check_link, links)
 
 while True:
     check_all()
